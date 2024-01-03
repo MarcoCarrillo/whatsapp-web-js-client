b19a5a8a7a2e626721812534078504e64f64be9e
Marco Carrillo <marcocarrilloosuna@gmail.com>
2024-01-03 12:04:25 -0600

    :rocket: feat: qr, create sessions and backend connection

--- src/App.js
+++ src/App.js
@@ -1,23 +1,71 @@
-import logo from './logo.svg';
 import './App.css';
+import io from 'socket.io-client';
+import QRCode from "react-qr-code";
+import { useEffect, useState } from 'react';
+const socket = io.connect('http://localhost:3001', {});
 
 function App() {
+  const [session, setSession] = useState('');
+  const [qrCode, setQrCode] = useState('');
+  const createSessionForWhatsapp = () => {
+    socket.emit('createSession', {
+      id: session
+    })
+  };
+
+  const [id, setId] = useState('');
+
+  useEffect(() => {
+    socket.emit('connected', 'Hello from client');
+    socket.on('qr', (data) => {
+      const { qr } = data;
+      setQrCode(qr);
+      console.log('QR Received');
+    });
+
+    socket.on('ready', (data) => {
+      console.log(data);
+      const { id } = data;
+      setId(id);
+    });
+
+    socket.on('allChats', (data) => {
+      console.log('all chaats', data);
+    })
+  }, []);
+
+  const getAllChats = () => {
+    socket.emit('getAllChats', { id })
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Cliente Whatsapp</h1>
+      <h2>Abre Whatsapp y escanea el codigo QR para iniciar sesion</h2>
+
+      <div>
+        <input
+          type='text'
+          value={session}
+          onChange={(e) => {
+            setSession(e.target.value);
+          }}
+          style={{
+            marginBottom: '20px'
+          }}
+        />
+        <button onClick={createSessionForWhatsapp}>Crear sesion</button>
+      </div>
+      <div
+        style={{
+          marginBottom: '20px'
+        }}
+      >
+        {id !== null &&
+          <button onClick={getAllChats}>Obtener chats</button>
+        }
+      </div>
+      <QRCode value={qrCode} />
+
     </div>
   );
 }
